992d2b8ff3da3e14f2304c778be35a7c83ab70cd
Jeremy <122048724+jer-j@users.noreply.github.com>
2023-11-04 16:56:09 -0400

    Update MomentOfInertiaCalc.m

--- MomentOfInertiaCalc.m
+++ MomentOfInertiaCalc.m
@@ -1,30 +1,30 @@
-% assume you get power data at a matrix of times and power
-% generate omega & alpha from calculations from Jermey's code
+% MomentOfInteriaCalc.m
+% generate omega & alpha from calculations from PowerOutputTesting.m
 % use this to output moment of inertia (avg of them all)
 
-clc;
-clear;
+% clc;
+% clear;
+load PowerOutputTesting.mat %dont use MOI
+pwrdata = readtable("2023-10-29-204844-FITNESS 1466-1-0-record.csv"); %update with file name
 
-% temporary example vectors
-t = [1;2;3;4;5;6;7;8;9;10]; % data from bike trainer, assumed collects faster than rpm
-T = [2;3;6;7]; % times bike pedal passes sensor (from arduino data)
-pwr = [1;2;1;4;3;1;3;5;1;4]; % power readings from trainer
-om = [2;4;5;6]; % omega values, calculated externally
-al = [1;2;-1;3]; % alpha values, calculated externally
+%fix data
+pwrdata.power(isnan(pwrdata.power)) = 0;
+pwrtimedata = datetime(extractBefore(convertCharsToStrings(pwrdata.timestamp),".000Z"));
+start = find(pwrdata.power,1);
+pwrtimewindow = pwrtimedata(start:end);
+t = seconds(time(between(pwrtimewindow(1), pwrtimewindow,"time")));
+
+pwr = pwrdata.power(start:end); % power readings from trainer
+% revTimes - times bike pedal passes sensor (from arduino data)
+% omega - angular velocity values (rad/s), calculated externally (from arduino data)
+% alpha - angular accelerationg values (rad/s^2), calculated externally (from arduino data)
 
 
-powerData = [t, pwr]; % where t is time col vector, pwr is data from machine
-omega = [T, om];
-alpha = [T, al];
-
-powerVals = powerData(:,2);
-indicies = ismember(powerData(:,1),alpha(:,1)); % t values that are in omega/alpha calcs
-
-% only the power values we need (where omega and alpha are calculated)
-newArray = powerVals(logical(indicies));
+indicies = ismembertol(t,revTimes,0.5); % t values that are in omega/alpha calcs
+timesOverlap = pwr(indicies); % only the power values we need (where omega and alpha are calculated)
 
 % get average of all MoI calcs (P/aw)
-MoI = mean(newArray.*(1./(omega(:,1).*alpha(:,1))));
+moi = mean(timesOverlap.*(1./(omega.*alpha)));
 
 % kg * m^2 bc power is in W (according to Ben)
-disp("Moment of Inertia: "+MoI+" kg * m^2")
+disp("Moment of Inertia: "+moi+" kg * m^2")
