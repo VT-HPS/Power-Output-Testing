2361d61d4f85fb47f0b97e8c06c1ecb5b6046e11
jer-j <122048724+jer-j@users.noreply.github.com>
2023-11-04 18:15:41 -0400

    reorganize sections

--- MomentOfInertiaCalc.m
+++ MomentOfInertiaCalc.m
@@ -4,23 +4,24 @@
 
 % clc;
 % clear;
-load PowerOutputTesting.mat %dont use MOI
+load PowerOutputTesting.mat revTimes omega alpha
+% revTimes - times bike pedal passes sensor from arduino data
+% omega - angular velocity values (rad/s), calculated from arduino data
+% alpha - angular accelerationg values (rad/s^2), calculated from arduino data
+
 pwrdata = readtable("2023-10-29-204844-FITNESS 1466-1-0-record.csv"); %update with file name
 
 %fix data
 pwrdata.power(isnan(pwrdata.power)) = 0;
 pwrtimedata = datetime(extractBefore(convertCharsToStrings(pwrdata.timestamp),".000Z"));
+
 start = find(pwrdata.power,1);
 pwrtimewindow = pwrtimedata(start:end);
-t = seconds(time(between(pwrtimewindow(1), pwrtimewindow,"time")));
+pwrTimes = seconds(time(between(pwrtimewindow(1), pwrtimewindow,"time")));
 
 pwr = pwrdata.power(start:end); % power readings from trainer
-% revTimes - times bike pedal passes sensor (from arduino data)
-% omega - angular velocity values (rad/s), calculated externally (from arduino data)
-% alpha - angular accelerationg values (rad/s^2), calculated externally (from arduino data)
 
-
-indicies = ismembertol(t,revTimes,0.5); % t values that are in omega/alpha calcs
+indicies = ismembertol(pwrTimes,revTimes,0.5); % t values that are in omega/alpha calcs
 timesOverlap = pwr(indicies); % only the power values we need (where omega and alpha are calculated)
 
 % get average of all MoI calcs (P/aw)
