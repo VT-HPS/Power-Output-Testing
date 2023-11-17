595dec51c17cf20ac22d83fe6bcacf521870dc67
jer-j <122048724+jer-j@users.noreply.github.com>
2023-11-16 22:00:53 -0500

    Spline Interpolation and curve fitting

--- PowerAnalysis.m
+++ PowerAnalysis.m
@@ -1,9 +1,10 @@
 % powerAnalysis.m
 radius = 0.3175; % m/s
-pwrdata = readtable("2023-11-13-201932-WAHOOAPPIOS2292-4-0 (4)-record.csv");
+pwrdata = readtable("2023-11-13-201932-WAHOOAPPIOS2292-4-0 (4)-record.csv",VariableNamingRule="modify");
 
 % P = torque * v/r 
 pwrdata.power(isnan(pwrdata.power)) = 0;
+pwrdata.enhancedSpeed(isnan(pwrdata.enhancedSpeed)) = 0;
 
 omega = pwrdata.enhancedSpeed ./ radius;
 
@@ -21,7 +22,7 @@ times = 0:1:seconds(pwrtimedata(end) - pwrtimedata(1));
 
 % figure;
 figure('Name',"Measured Values")
-tiledlayout(5,1)
+tiledlayout(4,1)
 
 nexttile
 plot(times,pwrdata.power)
@@ -41,30 +42,37 @@ nexttile
 plot(times,torque)
 title("torque")
 
-nexttile
-plot(times,MOI)
-title("MOI")
+% nexttile
+% plot(times,MOI)
+% title("MOI")
 
 % [~,Idx] = max(MOI);
 % 
 % moi = pwrdata.power(Idx) / (omega(Idx) * alpha(Idx));
-moi = 14.4411;
+moi = 7.33333333333;
 
 %% Derived Values
 revTimes = readmatrix("Onland_Testing_Data\KB\KB_1.txt");
-revTimes = revTimes(6:end);
+% revTimes = revTimes(4:end);
 revTimes = revTimes - revTimes(1);
-rpm = 60 ./ diff(revTimes);
-w = (pi/30) .* rpm;  % radians per second
+
+rpm = 60 ./ gradient(revTimes);
+w = ((pi/30) .* rpm) .* 1.1;  % radians per second
     
 % Spline Interpolation
-xq = revTimes(2):1:revTimes(end);
-ww = interp1(revTimes(2:end),w,xq,"linear");
+xq = linspace(revTimes(1),revTimes(end),numel(revTimes));
 
 
-a = gradient(ww, xq);  % radians per second squared​​
+% ww = interp1(revTimes,w,xq,"spline");
+
+ww = fit(revTimes,w,'smoothingspline','Normalize','on'); 
+
+W = feval(ww,xq);
+
+a = gradient(W);  % radians per second squared​​
 t = moi .* a;
-P = t .* ww;
+P = t .* W;
+P = abs(P) + abs(min(P));
 
 figure('Name',"Derived Values");
 tiledlayout(4,1)
@@ -78,8 +86,9 @@ ylabel("Power");
 
 % Plot w
 nexttile
-plot(revTimes(2:end), w,'o');
-plot(xq,ww);
+% plot(revTimes, w,'o');
+% plot(ww,xq);
+plot(ww,xq,w)
 title("\omega");
 xlabel("Time (s)");
 ylabel("rps");
@@ -100,13 +109,30 @@ xlabel("Time (s)");
 ylabel("Torque");
 
 
+%% %%
+% M = (pwrdata.power)' ./ (ww .* a);
+% m = (pwrdata.power) ./ (omega .* alpha);
+% M(isinf(M)|isnan(M)) = 0;
+% m(isinf(m)|isnan(m)) = 0;
+% cFa = mean(abs(M));
+% cFb = mean(abs(m));
+% figure;
+% tiledlayout(2,1)
+% nexttile
+% plot(times(1:118),M);
+% nexttile
+% plot(times,m)
 %%
-MOI = pwrdata.power ./ (omega .* alpha);
-figure;
-plot(times,MOI);
+w = (pi/30) .* rpm;  % radians per second
+    
+% Spline Interpolation
+xq = linspace(revTimes(1),revTimes(end),numel(revTimes));
 
-%%
 
-k = pwrdata.power(1:118) ./ (P');
-k(isinf(k)) = 0;
-convFactor = mean(abs(k));
+% ww = interp1(revTimes,w,xq,"spline");
+
+ww = fit(revTimes,w,'smoothingspline','Normalize','on');
+W = feval(ww,xq);
+
+figure;...
+plot(ww,xq,w)
